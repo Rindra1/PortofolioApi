34fc286aecbb710c821fe004ad3c43382f60782f
Rindra1 <rindraniaina.manda@gmail.com>
2025-10-03 15:37:57 +0300

    First commit

--- obj/Release/net8.0/PortofolioApi.AssemblyInfo.cs
+++ obj/Release/net8.0/PortofolioApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2555faf11d0d272bd5ac63d8207d9085a6267eff")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
