a6a8afb47cd25f1a428e3f1afb9248972bae76c9
Rindra1 <rindraniaina.manda@gmail.com>
2025-10-04 09:02:42 +0300

    update data

--- obj/Release/net8.0/PortofolioApi.AssemblyInfo.cs
+++ obj/Release/net8.0/PortofolioApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be0090072a7d467a7f956d066fcced64d8a51baf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff5638ab8d5522a56f1403b391d315d35868684a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
