55be03a30cee6316bf3723433368d6f593942a2b
Rindra1 <rindraniaina.manda@gmail.com>
2025-10-04 10:47:39 +0300

    le meme probleme

--- obj/Debug/net8.0/PortofolioApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortofolioApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0aa105121b7466a5531f0ac21e7573546a4802")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e378186a00c81c81361deb3e3edccd2d93dd1a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0/PortofolioApi.AssemblyInfo.cs
+++ obj/Release/net8.0/PortofolioApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed1de2696a877fb4d81d57d851dd8a87a4865e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e378186a00c81c81361deb3e3edccd2d93dd1a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
