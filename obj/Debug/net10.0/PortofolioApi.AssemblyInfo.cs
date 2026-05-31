c21c74d3058fd29f2553ff330f35ddb5bb0a6aa8
Rindra1 <rindraniaina.manda@gmail.com>
2026-05-31 09:03:58 +0300

    Update database

--- obj/Debug/net10.0/PortofolioApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/PortofolioApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1858c397687ad8d36d8cf6c4da4d816756e92950")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c5b3b5b628f80557ee94724192fc5197162c11b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortofolioApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
